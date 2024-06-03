1c5ac72f05dde641fbdc39dc54092e010bef5072
POFFIOLEG <144817552+rngblessed@users.noreply.github.com>
2024-06-04 00:37:43 +0300

    qwer

--- index.php
+++ index.php
@@ -4,19 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>POFFIZADANIE3</title>
-    <link rel="stylesheet" href="main.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/icheck-material@1.0.1/icheck-material.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/icheck-material@1.0.1/icheck-material-custom.min.css" />
+    <title>ZADANIE4</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" type="text/css"
-        href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
 
 </head>
 
@@ -182,64 +172,10 @@
                 </div>
             </form>
         </div>
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-
-        </br>
-        </br>
-        </br>
-        </br>
-        </br>
-        <div class="wrap2 lh-lg font-monospace">
-            <div class="box">
-                <ul class="list-unstyled-sm">
-                    <h5 id="links" class="text-center">Пункты выполненых скриншотов задания-3</h5>
-
-                    <li>
-                        <a>Подключение к общей базе данных;</a>
-                        </br>
-                        <img src="1.png"></img>
-                    </li>
-
-                    <li>
-                        <a>Использование личной базы данных</a>
-                        </br>
-                        <img src="2.png"></img>
-                    </li>
-                    <li>
-                        <a>Создание таблиц в базе данных;
-                        </a>
-                        </br>
-                        <img src="3.png"></img>
-
-                    <li>
-                        <a>Список таблиц в базе данных ;
-                        </a>
-                        </br>
-                        <img src="4.png"></img>
-                    </li>
-                    <li>
-                        <a>Результат заполнения формы и сохранения результатов в таблицы базы данных;</a>
-                        </br>
-                        <img src="6.png"></img>
-                    </li>
 
 
-
-                </ul>
-            </div>
-        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
